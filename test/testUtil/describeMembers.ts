1f04e548ba8930b1aa90f3dd3384dd98e9168204
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-10-22 17:43:03 +0200

    Fixed describeMembers callbacks not allowing null for custom properties

--- test/testUtil/describeMembers.ts
+++ test/testUtil/describeMembers.ts
@@ -1,13 +1,13 @@
-type Callback<T extends { constructor: Function }> = (instance: T) => void
+type Callback<T extends { constructor: Function }> = ((instance: T) => void) | null
 
-export function describeMembers<T extends { constructor: Function }, C extends { [P in keyof T]?: Callback<T> | null } & Record<string, Callback<T>>>(factory: () => T, callbacks: C) {
+export function describeMembers<T extends { constructor: Function }, C extends { [P in keyof T]?: Callback<T> } & Record<string, Callback<T>>>(factory: () => T, callbacks: C) {
     const instance = factory()
 
     describe(instance.constructor.name, () => {
         const members = [...Object.getOwnPropertyNames(instance), ...Object.getOwnPropertyNames(Object.getPrototypeOf(instance))]
         for (const key of members) {
             const name = "." + key
-            const callback = (callbacks as any)[key] as Callback<T> | null
+            const callback = (callbacks as any)[key] as Callback<T>
             if (callback != null) describe(name, () => callback(factory()))
             else if (name[0] == "." && callback !== null) it(name)
         }
