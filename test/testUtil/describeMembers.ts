4df2f22114a2fb0c63410e1567ce645624579b43
Branislav Trstenský <bt7s7k7@hotmail.sk>
2020-10-22 17:39:17 +0200

    Added a tracker

--- test/testUtil/describeMembers.ts
+++ test/testUtil/describeMembers.ts
@@ -1,11 +1,13 @@
-export function describeMembers<T extends { constructor: Function }, C extends { [P in keyof T]?: ((instance: T) => void) | null }>(factory: () => T, callbacks: C) {
+type Callback<T extends { constructor: Function }> = (instance: T) => void
+
+export function describeMembers<T extends { constructor: Function }, C extends { [P in keyof T]?: Callback<T> | null } & Record<string, Callback<T>>>(factory: () => T, callbacks: C) {
     const instance = factory()
 
     describe(instance.constructor.name, () => {
         const members = [...Object.getOwnPropertyNames(instance), ...Object.getOwnPropertyNames(Object.getPrototypeOf(instance))]
         for (const key of members) {
             const name = "." + key
-            const callback = (callbacks as any)[key] as ((instance: T) => void) | null
+            const callback = (callbacks as any)[key] as Callback<T> | null
             if (callback != null) describe(name, () => callback(factory()))
             else if (name[0] == "." && callback !== null) it(name)
         }
---
+++ test/testUtil/tracker.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai"
+
+export function tracker(name: string) {
+    return {
+        count: 0,
+        trigger() {
+            this.count++
+        },
+        check(amount = 1) {
+            expect(this.count).to.equal(amount, `Tracker "${name}" has not beed triggered the required amount of times`)
+        }
+    }
+}
